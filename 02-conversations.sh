b849a5998a3e1b4ab1dab782addcd6d8b9534678
deluri <dileep.apps17@gmail.com>
2025-05-24 23:33:51 +0530

    hello world started

---
+++ 02-conversations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Dhruva: How are you?"
+echo "Sahasra: I am fine, how are you?"
+echo "Dhruva: I am fine, thanks for asking?"
+echo "Sahasra: How was your day?"
+echo "Dhruva: It was great"
