8c0c74e17fdbb7b04d6cb7f2194b10838ad416d6
deluri <dileep.apps17@gmail.com>
2025-05-24 23:38:51 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=DHRUVA
+PERSON2=SAHASRA
+
+echo "$PERSON1: How are you?"
+echo "$PERSON2: I am fine, how are you?"
+echo "$PERSON1: I am fine, thanks for asking?"
+echo "$PERSON2: How was your day?"
+echo "$PERSON1: It was great"
