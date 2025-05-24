b331767253e0eff8f15f7577cdc124066fdb7272
deluri <dileep.apps17@gmail.com>
2025-05-24 23:47:42 +0530

    variabls args

---
+++ 04-variables-args.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: How are you?"
+echo "$PERSON2: I am fine, how are you?"
+echo "$PERSON1: I am fine, thanks for asking?"
+echo "$PERSON2: How was your day?"
+echo "$PERSON1: It was great"
