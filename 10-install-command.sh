c2cca4ab5a4b2e8a15ecdfe07594f727f59419ae
deluri <dileep.apps17@gmail.com>
2025-05-25 13:03:48 +0530

    install commands

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -10,6 +10,11 @@ else
     echo "You are running with root user"
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MYSQL is not installed...going to install MySQl"
 dnf install mysql -y
 
 if [ $? -eq 0 ]
@@ -19,3 +24,16 @@ else
     echo "Installing MySql is....FAILURE"
     exit 1
 fi
+else
+    echo "MYSQL is already installed..nothing to do"
+fi
+
+# dnf install mysql -y
+
+# if [ $? -eq 0 ]
+# then
+#     echo "Installing MySql is....SUCCESS"
+# else
+#     echo "Installing MySql is....FAILURE"
+#     exit 1
+# fi
