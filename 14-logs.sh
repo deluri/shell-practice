ae654468f9a9a8fe0fe88ef1a89475733489bfec
deluri <dileep.apps17@gmail.com>
2025-05-26 23:52:29 +0530

    logs

--- 14-logs.sh
+++ 14-logs.sh
@@ -12,7 +12,7 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER
 echo "Script execution started at :: $(date)" | tee -a $LOG_FILE
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ];
 then
     echo -e "$R ERROR: Please run this script with root user $N" | tee -a $LOG_FILE
     exit 1
@@ -21,7 +21,7 @@ else
 fi
 
 VALIDATE(){
-   if [ $1 -eq 0 ]
+   if [ $1 -eq 0 ];
    then
        echo -e "Installing $2 is....$G SUCCESS $N" | tee -a $LOG_FILE
    else
@@ -32,7 +32,7 @@ VALIDATE(){
 
 dnf list installed mysql &>>$LOG_FILE
 
-if [ $? -ne 0 ]
+if [ $? -ne 0 ];
 then
     echo "MYSQL is not installed...going to install MySQl" | tee -a $LOG_FILE
 dnf install mysql -y &>>$LOG_FILE
@@ -40,17 +40,21 @@ VALIDATE $? "mysql"
 else
     echo -e "nothing to do MYSQL...$Y already installed $N" | tee -a $LOG_FILE
 fi
+
 dnf list installed python3 &>>$LOG_FILE
-if [ $? -ne 0 ]
+if [ $? -ne 0 ];
 then
     echo "python3 is not installed...going to install python3" | tee -a $LOG_FILE
+
 dnf install Python3 -y &>>$LOG_FILE
 VALIDATE $? "python3"
 else
     echo -e "nothing to do python...$Y already installed $N" | tee -a $LOG_FILE
 fi
+
+
 dnf list installed nginx &>>$LOG_FILE
-if [ $? -ne 0 ]
+if [ $? -ne 0 ];
 then
     echo "nginx is not installed...going to install nginx" | tee -a $LOG_FILE
 dnf install nginx -y &>>$LOG_FILE
