d6996d7855e13a890edc99f7324b7c38a9662f3a
deluri <dileep.apps17@gmail.com>
2025-05-26 23:27:49 +0530

    logs

--- 14-logs.sh
+++ 14-logs.sh
@@ -7,7 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "Script execution started at :: $(date)" | tee -a $LOG_FILE
